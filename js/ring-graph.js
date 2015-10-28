89209ebb5c64c81a9de7513a5b1c2739067e8de2
Francesco Bedussi <francesco@francescobedussi.it>
2015-10-28 12:53:25 +0100

    Init pie chart

---
+++ js/ring-graph.js
@@ -0,0 +1,42 @@
+(function() {
+    "use strict";
+    
+    function drawPie() {
+        [].forEach.call(document.querySelectorAll('.pie'), function(el) {
+            var value = el.dataset.value,
+                less50 = el.getElementsByClassName('less-than-50')[0],
+                more50 = el.getElementsByClassName('more-than-50')[0];
+        
+            var prefix = (function() {
+              var docEl = document.documentElement, s;
+              if (docEl && (s = docEl.style)) {
+                  if (typeof s.MozRotate == "string") {
+                    return "-moz-";
+                  } else if (typeof s.WebkitRotate == "string") {
+                    return "-webkit-";
+                  } else if (typeof s.MsRotate == "string") {
+                    return "-ms-";
+                  }
+                            return "";
+              }
+            })();
+        
+            //console.log(prefix);
+        
+        
+            if (value <=50) {
+              var d = -180 + (value * (180/50));
+              more50.style.display = 'none';
+              less50.style.transform = prefix+"rotate("+d+"deg)";
+            }
+        
+            if (value >50) {
+              var d = (value * (180/50));
+              more50.style.display = 'block';
+              less50.style.transform = prefix+"rotate(0deg)";
+              more50.style.transform = prefix+"rotate("+d+"deg)";
+        }
+        });
+    }        
+    window.ringGraph = drawPie;
+})()
